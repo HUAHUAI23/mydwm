a384d406db845f96558cae97fe6b87388c70ad53
HUAHUAI23 <1319873800@qq.com>
2020-04-03 19:37:10 +0800

    change font size and change borderpix

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 8;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 8;       /* vert inner gap between windows */
@@ -13,8 +13,8 @@ static const int smartgaps          = 1;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "Sauce Code Pro Nerd Font Complete:size=16" };
-static const char dmenufont[]       = "Sauce Code Pro Nerd Font Complete:size=16";
+static const char *fonts[]          = { "Sauce Code Pro Nerd Font Complete:size=12" };
+static const char dmenufont[]       = "Sauce Code Pro Nerd Font Complete:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
